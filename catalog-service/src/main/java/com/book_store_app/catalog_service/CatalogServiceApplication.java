50ca234352323977725c4407e47a650bc4c097ca
Ravindu Jayaweera <ravindujayaweera99@Ravindus-MacBook-Air.local>
2026-03-20 23:29:57 +0530

    Root project created and catalog service added

---
+++ catalog-service/src/main/java/com/book_store_app/catalog_service/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.book_store_app.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
